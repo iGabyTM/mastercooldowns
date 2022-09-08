16e74c0a83c6e99846ada8a3859ba3fc8d1bdbbd
Gabriel Dumitru <contactgabytm@gmail.com>
2022-09-08 13:41:33 +0300

    fix typo in TimeUtil

--- src/main/java/me/gabytm/mastercooldowns/utils/TimeUtil.java
+++ src/main/java/me/gabytm/mastercooldowns/utils/TimeUtil.java
@@ -1,22 +1,20 @@
 /*
+ * Copyright 2019 GabyTM
  *
- * PlaceholderAPI
- * Copyright (C) 2019 Ryan McCarthy
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ *  permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
 package me.gabytm.mastercooldowns.utils;
@@ -24,38 +22,37 @@ package me.gabytm.mastercooldowns.utils;
 import me.gabytm.mastercooldowns.MasterCooldowns;
 import org.bukkit.configuration.file.FileConfiguration;
 
+import java.util.StringJoiner;
+
 public class TimeUtil {
 
     private static final MasterCooldowns plugin = MasterCooldowns.getPlugin(MasterCooldowns.class);
 
-    private static void append(final StringBuilder builder, final long duration, final String prefix) {
+    private static void append(final StringJoiner joiner, final long duration, final String prefix) {
         if (duration == 0) {
             return;
         }
 
-        if (builder.length() > 0) {
-            builder.append(' ');
-        }
-
-        builder.append(duration).append(prefix);
+        joiner.add(duration + prefix);
     }
 
-    public static String format(final long time) {
+    public static String format(final long seconds) {
         final FileConfiguration config = plugin.getConfig();
 
-        if (time < 60) return time + config.getString("placeholders.formatted.days", "d");
+        if (seconds < 60) {
+            return seconds + config.getString("placeholders.formatted.seconds", "s");
+        }
 
-        final long seconds = time;
         final long minutes = seconds / 60;
         final long hours = minutes / 60;
         final long days = hours / 24;
 
-        final StringBuilder builder = new StringBuilder();
-        append(builder, days, config.getString("placeholders.formatted.days", "d"));
-        append(builder, hours % 24, config.getString("placeholders.formatted.hours", "h"));
-        append(builder, minutes % 60, config.getString("placeholders.formatted.minutes", "m"));
-        append(builder, seconds % 60, config.getString("placeholders.formatted.seconds", "s"));
-
-        return builder.toString();
+        final StringJoiner joiner = new StringJoiner(" ");
+        append(joiner, days, config.getString("placeholders.formatted.days", "d"));
+        append(joiner, hours % 24, config.getString("placeholders.formatted.hours", "h"));
+        append(joiner, minutes % 60, config.getString("placeholders.formatted.minutes", "m"));
+        append(joiner, seconds % 60, config.getString("placeholders.formatted.seconds", "s"));
+        return joiner.toString();
     }
+
 }
